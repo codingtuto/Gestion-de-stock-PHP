cdf54eb4d915c6f36b4f4a7116e7e1b7c5edbf8f
Coding Team <87824092+codingtuto@users.noreply.github.com>
2022-03-28 18:32:29 +0000

    bugs fixes

--- editer.php
+++ editer.php
@@ -13,7 +13,7 @@ if(isset($_POST['update_product'])){
    $product_image_folder = 'uploaded_img/'.$product_image;
 
    if(empty($product_name) || empty($product_price) || empty($product_image)){
-      $message[] = 'please fill out all!';    
+      $message[] = 'Tous les champs sont requis!';    
    }else{
 
       $update_data = "UPDATE products SET name='$product_name', price='$product_price', image='$product_image'  WHERE id = '$id'";
@@ -21,9 +21,9 @@ if(isset($_POST['update_product'])){
 
       if($upload){
          move_uploaded_file($product_image_tmp_name, $product_image_folder);
-         header('location:admin_page.php');
+         header('location:index.php');
       }else{
-         $$message[] = 'please fill out all!'; 
+         $$message[] = 'Champs obligatoire!'; 
       }
 
    }
@@ -62,7 +62,7 @@ if(isset($_POST['update_product'])){
    ?>
    
    <form action="" method="post" enctype="multipart/form-data">
-      <h3 class="title">update the product</h3>
+      <h3 class="title">Mettre à jour le produit</h3>
       <input type="text" class="box" name="product_name" value="<?php echo $row['name']; ?>" placeholder="entrer le nom du produit">
       <input type="number" min="0" class="box" name="product_price" value="<?php echo $row['price']; ?>" placeholder="entrer le prix du produit">
       <input type="file" class="box" name="product_image"  accept="image/png, image/jpeg, image/jpg">
